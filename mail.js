bafcbc3055b7ba5d533fba254e9d3e9fcd94aead
Samarth Gupta <gsamarth14@gmail.com>
2022-11-21 04:38:55 +0530

    Completed Backend Functionalities

--- mail.js
+++ mail.js
@@ -1,43 +1,16 @@
 const express = require("express");
+const fs = require("fs");
 
+const handlebars = require("handlebars");
 const app = express();
 
 const bodyParser = require("cookie-parser");
 
+const nodemailer = require("nodemailer");
+
 app.use(bodyParser({ extended: true }));
 
 app.use(express.json());
-
-const nodemailer = require("nodemailer");
-
-async function main(addressesList) {
-  let transporter = nodemailer.createTransport({
-    service: "hotmail",
-    auth: {
-      user: "herculesproject7@outlook.com",
-      pass: "hercules7@7",
-    },
-  });
-
-  await transporter.sendMail(
-    {
-      from: "herculesproject7@outlook.com",
-      to: addressesList,
-      subject: "Booking Confirmation",
-      html: {
-        path: "index.html",
-      }, // html body
-    },
-    (err, inf) => {
-      console.log(err);
-      console.log(inf);
-    }
-  );
-}
-
-app.get("/", (req, res) => {
-  res.send("<h1> Hello World");
-});
 
 app.post("/sendEmail", async (req, res) => {
   try {
@@ -45,7 +18,39 @@ app.post("/sendEmail", async (req, res) => {
 
     const addressesList = req.body.addressList;
 
+    const messageString = req.body.messageString;
+
     console.log(addressesList);
+
+    const filePath = "index.html";
+    const source = fs.readFileSync(filePath, "utf-8").toString();
+    const template = handlebars.compile(source);
+
+    const replacements = {
+      tomail: messageString,
+    };
+    const htmlToSend = template(replacements);
+
+    let transporter = nodemailer.createTransport({
+      service: "hotmail",
+      auth: {
+        user: "herculesproject7@outlook.com",
+        pass: "hercules7@7",
+      },
+    });
+
+    await transporter.sendMail(
+      {
+        from: "herculesproject7@outlook.com",
+        to: addressesList,
+        subject: "Booking Confirmation",
+        html: htmlToSend, // html body
+      },
+      (err, inf) => {
+        console.log(err);
+        console.log(inf);
+      }
+    );
 
     res.json({
       message: "Your Bookings is Confirmed, Email is sent successfully.",
--- var.js
+++ var.js
@@ -1,2 +1,47 @@
+import express, { Application, Request, Response } from "express";
+import nodemailer from "nodemailer";
+import handlebars from "handlebars";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: "email",
+    pass: "password",
+  },
+  logger: true,
+});
+
+const app = express();
+
+const port = 8000;
+
+app.get("/", async (req, res) => {
+  const filePath = path.join(__dirname, "../emails/template.html");
+  const source = fs.readFileSync(filePath, "utf-8").toString();
+  const template = handlebars.compile(source);
+  const replacements = {
+    username: "Darth Vader",
+  };
+  const htmlToSend = template(replacements);
+
+  const info = await transporter.sendMail({
+    from: '"Sender Name" <from@example.net>',
+    to: "email.address@gmail.com",
+    subject: "Hello from node",
+    text: "Hello world?",
+    html: htmlToSend,
+    headers: { "x-myheader": "test header" },
+  });
+
+  console.log("Message sent: %s", info.response);
+  res.send("Hello world!");
+});
+
+app.listen(port, function () {
+  console.log(`App is listening on port ${port} !`);
+});
 
 
